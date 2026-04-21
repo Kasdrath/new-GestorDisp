a25d0a1b3de967c936ed170a8c3f1699b2edfe19
Kasdrath <96744522+Kasdrath@users.noreply.github.com>
2026-04-21 10:18:22 -0400

    .

--- frontend/src/app/services/dispositivoService.ts
+++ frontend/src/app/services/dispositivoService.ts
@@ -1,10 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DispositivoService {
+  private apiUrl = 'http://localhost:8080/api/dispositivos';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+  obtenerTodos(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  obtenerPorId(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+
+  crear(dispositivo: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, dispositivo);
+  }
+
+  actualizar(id: number, dispositivo: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${id}`, dispositivo);
+  }
+
+  eliminar(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+  }
 }
